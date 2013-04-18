3aa8bdf3c7bb1678f4dda30d35a6a6a282867c07
John-Philip Johansson <john-philipjohansson@John-Philip-Johanssons-MacBook-Pro.local>
2013-04-19 00:37:37 +0200

    Adding existence code

--- dist/jquery.existence.js
+++ dist/jquery.existence.js
@@ -1,3 +1,6 @@
+/*! jQuery Existence - v0.1.0 - 2013-04-19
+* https://github.com/seriema/jquery.existence
+* Copyright (c) 2013 John-Philip Johansson; Licensed MIT */
 (function ($) {
     'use strict';
 
--- test/jquery.existence_test.js
+++ test/jquery.existence_test.js
@@ -1,62 +1,23 @@
 (function($) {
-  /*
-    ======== A Handy Little QUnit Reference ========
-    http://api.qunitjs.com/
+  'use strict';
 
-    Test methods:
-      module(name, {[setup][ ,teardown]})
-      test(name, callback)
-      expect(numberOfAssertions)
-      stop(increment)
-      start(decrement)
-    Test assertions:
-      ok(value, [message])
-      equal(actual, expected, [message])
-      notEqual(actual, expected, [message])
-      deepEqual(actual, expected, [message])
-      notDeepEqual(actual, expected, [message])
-      strictEqual(actual, expected, [message])
-      notStrictEqual(actual, expected, [message])
-      throws(block, [expected], [message])
-  */
+  module('jQuery.existence exists');
 
-  module('jQuery#awesome', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
+  test('Simple element exists', function () {
+    ok($('#has-children').exists());
   });
 
-  test('is chainable', function() {
-    expect(1);
-    // Not a bad test to run on collection methods.
-    strictEqual(this.elems.awesome(), this.elems, 'should be chainable');
+  test('Simple parent has children', function () {
+     ok($('#has-children').children().exists());
   });
 
-  test('is awesome', function() {
-    expect(1);
-    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
+  module('jQuery.existence missing');
+
+  test('Simple element missing', function () {
+     ok($('#fake-id').missing());
   });
 
-  module('jQuery.awesome');
-
-  test('is awesome', function() {
-    expect(2);
-    strictEqual($.awesome(), 'awesome.', 'should be awesome');
-    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
+  test('Simle fake element has no children', function () {
+     ok($('#fake-id').children().missing());
   });
-
-  module(':awesome selector', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
-
-  test('is awesome', function() {
-    expect(1);
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
-  });
-
 }(jQuery));
